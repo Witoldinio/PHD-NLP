744031493038200c653848375dc3ba1e8c8114d8
zywica <witold.zywica@nokia.com>
2020-10-24 15:00:36 +0200

    Initial commit

---
+++ NLP/Controllers/TokenizerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenNLP.Tools.Tokenize;
+
+namespace NLP.Controllers
+{
+    public class TokenizerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Tokenize(string sentence)
+        {
+            //// Regular tokenizer
+            //var modelPath = "path/to/EnglishTok.nbin";
+            //var sentence = "- Sorry Mrs. Hudson, I'll skip the tea.";
+            //var tokenizer = new EnglishMaximumEntropyTokenizer(modelPath);
+            //var tokens = tokenizer.Tokenize(sentence);
+            //// tokens = ["-", "Sorry", "Mrs.", "Hudson", ",", "I", "'ll", "skip", "the", "tea", "."]
+
+            // English tokenizer
+            EnglishRuleBasedTokenizer tokenizer = new EnglishRuleBasedTokenizer(false);
+            //var sentence = "- Sorry Mrs. Hudson, I'll skip the tea.";
+            var tokens = tokenizer.Tokenize(sentence);
+            // tokens = ["-", "Sorry", "Mrs.", "Hudson", ",", "I", "'ll", "skip", "the", "tea", "."]
+
+            return new JsonResult(tokens);
+        }
+    }
+}
